7a49c993daf1dca203706bb7d7477da5204ab2c0
walid <walidkhalid217@gmail.com>
2025-08-26 19:35:15 +0300

    Added import statements and constants to spaceshooter.py

--- spaceshooter.py
+++ spaceshooter.py
@@ -22,3 +22,21 @@ Yellow_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png"))), (WIDTH, HEIGHT)
 
+class Laser:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    def move(self, vel):
+        self.y += vel
+
+    def off_screen(self, height):
+        return not(self.y <= height and self.y >= 0)
+    
+    def collision(self, obj):
+        return collide(self, obj)
