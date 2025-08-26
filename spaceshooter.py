65ead46990e1744a9f632d048ab8a1fa51c32281
walid <walidkhalid217@gmail.com>
2025-08-26 19:50:08 +0300

    added functions to Ship class

--- spaceshooter.py
+++ spaceshooter.py
@@ -67,3 +67,23 @@ class Ship:
                 obj.health -= 10
                 self.lasers.remove(laser)
 
+    def cooldown(self):
+        if self.cool_down_counter >= self.COOLDOWN:
+            self.cool_down_counter = 0
+        elif self.cool_down_counter > 0:
+            self.cool_down_counter += 1
+
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            laser = Laser(self.x, self.y, self.laser_img)
+            self.lasers.append(laser)
+            self.cool_down_counter = 1
+
+    def get_width(self):
+        return self.ship_img.get_width()
+    
+    def get_height(self):
+        return self.ship_img.get_height()
+    
+    
+
