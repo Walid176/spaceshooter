eb75f5c21a2ecb2725282371a0d83ad0acd8ae50
walid <walidkhalid217@gmail.com>
2025-08-26 19:29:38 +0300

    Added files to assets folder

---
+++ spaceshooter.py
@@ -0,0 +1,24 @@
+import pygame
+import os
+import time
+import random
+pygame.font.init()
+
+WIDTH = 750
+HEIGHT = 750
+WIN =  pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Shooter")
+
+Red_Space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+Green_Space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+Blue_Space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+
+Yellow_space_Ship = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
+
+Red_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+Blue_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+Green_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+Yellow_Laser = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png"))), (WIDTH, HEIGHT)
+
