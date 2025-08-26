a3d68d41ee6284a66692e13d9944843bbd022d60
walid <walidkhalid217@gmail.com>
2025-08-26 19:42:09 +0300

    Created Laser class

--- spaceshooter.py
+++ spaceshooter.py
@@ -40,3 +40,30 @@ class Laser:
     
     def collision(self, obj):
         return collide(self, obj)
+    
+class Ship:
+    COOLDOWN = 30
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health  = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        window.blit(self.ship_img, (self.x, self.y))
+        for laser in self.lasers:
+            laser.draw(window)
+
+    def move_lasers(self, vel, obj):
+        self.cooldown()
+        for laser in self.lasers:
+            laser.move(vel)
+            if laser.off_screen(HEIGHT):
+                self.lasers.remove(laser)
+            elif laser.collision(obj):
+                obj.health -= 10
+                self.lasers.remove(laser)
+
